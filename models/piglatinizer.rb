b5dee2a0e481650c5927a8df091f2c18438fbf94
Madeline Stark <madeline.stark1@gmail.com>
2018-09-11 21:57:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -53,7 +53,7 @@ class PigLatinizer
         end
         if new_word == ["u", "n", "t", "i", "l"] || new_word == ["e", "n", "u", "m", "e", "r", "a", "t", "i", "o", "n"]
           text_arrayed_ay = new_word << ["w", "a", "y"]
-        elsif vowels.include?(first.downcase)
+        elsif vowels.include?(first)
           text_arrayed_ay = new_word << ["w", "a", "y"]
         else
           text_arrayed_ay = new_word << ["a", "y"]
