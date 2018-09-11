cec397f7b04dbf0de727f7f9d2ad1116db4bb278
Madeline Stark <madeline.stark1@gmail.com>
2018-09-11 21:17:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    text_from_user = params[:user_text]
+    text_from_user = params[:user_phrase]
     @analyzed_text = PigLatinizer.new.piglatinize(text_from_user)
     erb :user_input
   end
